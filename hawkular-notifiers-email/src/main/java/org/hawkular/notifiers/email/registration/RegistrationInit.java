be207cc33f6e2ac09541ef0f79e105fa469cb3cb
Lucas Ponce <ponce.ballesteros@gmail.com>
2015-02-04 19:22:07 +0100

    HAWKULAR-14 Fix for initialization of notifiers plugin

--- hawkular-notifiers-email/src/main/java/org/hawkular/notifiers/email/registration/RegistrationInit.java
+++ hawkular-notifiers-email/src/main/java/org/hawkular/notifiers/email/registration/RegistrationInit.java
@@ -25,12 +25,12 @@ import org.hawkular.notifiers.api.log.MsgLogger;
 import org.hawkular.notifiers.api.model.NotifierTypeRegistrationMessage;
 import org.jboss.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.jms.JMSException;
 import javax.jms.QueueConnectionFactory;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -40,8 +40,9 @@ import java.util.Set;
  * @author Jay Shaughnessy
  * @author Lucas Ponce
  */
-@WebListener
-public class RegistrationInit implements ServletContextListener {
+@Startup
+@Singleton
+public class RegistrationInit {
     private final MsgLogger msgLog = MsgLogger.LOGGER;
     private final Logger log = Logger.getLogger(RegistrationInit.class);
     private static final String NOTIFIER_TYPE_REGISTER = "NotifierTypeRegisterQueue";
@@ -51,13 +52,8 @@ public class RegistrationInit implements ServletContextListener {
     private ConnectionContextFactory ccf;
     private ProducerConnectionContext pcc;
 
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        msgLog.infoPluginUnregistered("email");
-    }
-
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
+    @PostConstruct
+    public void init() {
         try {
             ccf = new ConnectionContextFactory(conFactory);
             pcc = ccf.createProducerConnectionContext(new Endpoint(Endpoint.Type.QUEUE, NOTIFIER_TYPE_REGISTER));
