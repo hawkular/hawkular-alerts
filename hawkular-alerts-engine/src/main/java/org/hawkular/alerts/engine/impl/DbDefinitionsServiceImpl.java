2d68ee013281c36d8a9d47325509dc7d44538c60
Lucas Ponce <ponce.ballesteros@gmail.com>
2015-03-16 12:21:15 +0100

    Fix action plugins query

--- hawkular-alerts-engine/src/main/java/org/hawkular/alerts/engine/impl/DbDefinitionsServiceImpl.java
+++ hawkular-alerts-engine/src/main/java/org/hawkular/alerts/engine/impl/DbDefinitionsServiceImpl.java
@@ -1293,7 +1293,7 @@ public class DbDefinitionsServiceImpl implements DefinitionsService {
             c = ds.getConnection();
             s = c.createStatement();
 
-            StringBuilder sql = new StringBuilder("SELECT actionPlugin FROM HWK_ALERTS_ACTION_PLUGIN ")
+            StringBuilder sql = new StringBuilder("SELECT actionPlugin FROM HWK_ALERTS_ACTION_PLUGINS ")
                     .append("ORDER BY actionPlugin");
             log.debugf("SQL: " + sql);
             rs = s.executeQuery(sql.toString());
