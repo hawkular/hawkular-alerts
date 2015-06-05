357e18a106a861cb1db083e20ea552e6b69ec4a9
Lucas Ponce <ponce.ballesteros@gmail.com>
2015-06-05 11:43:57 +0200

    HWKALERTS-19 Pagination model

---
+++ hawkular-alerts-api/src/main/java/org/hawkular/alerts/api/model/paging/PageContext.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.alerts.api.model.paging;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Lukas Krejci
+ * @since 0.0.1
+ */
+public class PageContext {
+    public static final int UNLIMITED_PAGE_SIZE = -1;
+
+    private final int pageSize;
+    private final int pageNumber;
+    private final List<Order> order;
+
+    public PageContext(int pageNumber, int pageSize, Order... orders) {
+        if (orders.length == 0) {
+            throw new IllegalArgumentException("At least one order specification must be supplied.");
+        }
+        this.pageNumber = pageSize >= 0 ? pageNumber : 0;
+        this.pageSize = pageSize;
+        List<Order> tmp = new ArrayList<>();
+        Collections.addAll(tmp, orders);
+        this.order = Collections.unmodifiableList(tmp);
+    }
+
+    public PageContext(int pageNumber, int pageSize, Iterable<Order> orders) {
+        this.pageNumber = pageSize >= 0 ? pageNumber : 0;
+        this.pageSize = pageSize;
+        List<Order> tmp = new ArrayList<>();
+        orders.forEach(tmp::add);
+        if (tmp.size() == 0) {
+            throw new IllegalArgumentException("At least one order specification must be supplied.");
+        }
+
+        this.order = Collections.unmodifiableList(tmp);
+    }
+
+    /**
+     * @return the number of the page this context represents (0-based)
+     */
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    /**
+     * The associated {@link Page} objects should contain at most this number of elements.
+     *
+     * @return the page size that is being used
+     */
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    /**
+     * This trivially translates to {@code pageNumber * pageSize}.
+     *
+     * @return the index of the first element in the overall results that belongs to the page designated by this context
+     */
+    public int getStart() {
+        return pageNumber * pageSize;
+    }
+
+    /**
+     * If this page context is {@link #isLimited() limited}, then this returns {@code getStart() + pageSize}. If this
+     * page context IS unlimited, {@link Integer#MAX_VALUE} is returned.
+     *
+     * @return the index just after the last element in the overall results the page designated by this context
+     * represents
+     */
+    public int getEnd() {
+        if (isLimited()) {
+            return getStart() + pageSize;
+        } else {
+            return Integer.MAX_VALUE;
+        }
+    }
+
+    /**
+     * Page size less then zero represents no limit on the size of the page. Effectively, page contexts like that will
+     * always represent the first (and only) page of the results.
+     *
+     * @return true if the page size is less than zero
+     */
+    public boolean isLimited() {
+        return pageSize >= 0;
+    }
+
+    /**
+     * This list of ordering fields.
+     *
+     * @return the unmodifiable list of ordering fields.
+     */
+    public List<Order> getOrder() {
+        return order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PageContext)) return false;
+
+        PageContext that = (PageContext) o;
+
+        return pageSize == that.pageSize && pageNumber == that.pageNumber && this.order.equals(that.order);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = pageSize;
+        result = 31 * result + pageNumber;
+        return result;
+    }
+
+    @Override public String toString() {
+        return "PagingState[" + "order=" + order + ", pageNumber=" + pageNumber + ", pageSize=" +
+                pageSize + ']';
+    }
+
+}
--- hawkular-alerts-rest/src/main/java/org/hawkular/alerts/rest/ResponseUtil.java
+++ hawkular-alerts-rest/src/main/java/org/hawkular/alerts/rest/ResponseUtil.java
@@ -18,9 +18,16 @@ package org.hawkular.alerts.rest;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import org.hawkular.alerts.api.model.paging.Page;
+import org.hawkular.alerts.api.model.paging.PageContext;
+import org.hawkular.alerts.rest.json.Link;
 
 /**
  * Helper class used to build REST responses and deal with errors.
@@ -51,6 +58,18 @@ public class ResponseUtil {
         return Response.status(Response.Status.OK).entity(entity).type(APPLICATION_JSON_TYPE).build();
     }
 
+    public static <T> Response paginatedOk(Page<T> page, UriInfo uri) {
+
+        //extract the data out of the page
+        List<T> data = new ArrayList<>(page);
+
+        Response.ResponseBuilder response = Response.status(Response.Status.OK).entity(data);
+
+        createPagingHeader(response, uri, page);
+
+        return response.build();
+    }
+
     public static Response ok() {
         return Response.status(Response.Status.OK).type(APPLICATION_JSON_TYPE).build();
     }
@@ -60,5 +79,67 @@ public class ResponseUtil {
         errors.put("errorMsg", "Bad request: " + message);
         return Response.status(Response.Status.BAD_REQUEST)
                 .entity(errors).type(APPLICATION_JSON_TYPE).build();
+    }
+
+    /**
+     * Create the paging headers for collections and attach them to the passed builder. Those are represented as
+     * <i>Link:</i> http headers that carry the URL for the pages and the respective relation.
+     * <br/>In addition a <i>X-Total-Count</i> header is created that contains the whole collection size.
+     *
+     * @param builder    The ResponseBuilder that receives the headers
+     * @param uriInfo    The uriInfo of the incoming request to build the urls
+     * @param resultList The collection with its paging information
+     */
+    public static void createPagingHeader(final Response.ResponseBuilder builder, final UriInfo uriInfo,
+            final Page<?> resultList) {
+
+        UriBuilder uriBuilder;
+
+        PageContext pc = resultList.getPageContext();
+        int page = pc.getPageNumber();
+
+        List<Link> links = new ArrayList<>();
+
+        if (pc.isLimited() && resultList.getTotalSize() > (pc.getPageNumber() + 1) * pc.getPageSize()) {
+            int nextPage = page + 1;
+            uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
+            uriBuilder.replaceQueryParam("page", nextPage);
+
+            links.add(new Link("next", uriBuilder.build().toString()));
+        }
+
+        if (page > 0) {
+            int prevPage = page - 1;
+            uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
+            uriBuilder.replaceQueryParam("page", prevPage);
+            links.add(new Link("prev", uriBuilder.build().toString()));
+        }
+
+        // A link to the last page
+        if (pc.isLimited()) {
+            long lastPage = resultList.getTotalSize() / pc.getPageSize();
+            if (resultList.getTotalSize() % pc.getPageSize() == 0) {
+                lastPage -= 1;
+            }
+
+            uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
+            uriBuilder.replaceQueryParam("page", lastPage);
+            links.add(new Link("last", uriBuilder.build().toString()));
+        }
+
+        // A link to the current page
+        uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
+
+        StringBuilder linkHeader = new StringBuilder(new Link("current", uriBuilder.build().toString())
+                .rfc5988String());
+
+        //followed by the rest of the link defined above
+        links.forEach((l) -> linkHeader.append(", ").append(l.rfc5988String()));
+
+        //add that all as a single Link header to the response
+        builder.header("Link", linkHeader.toString());
+
+        // Create a total size header
+        builder.header("X-Total-Count", resultList.getTotalSize());
     }
 }
