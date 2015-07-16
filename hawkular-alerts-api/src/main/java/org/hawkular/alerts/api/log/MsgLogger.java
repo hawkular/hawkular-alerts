f1d029ccb419855f673698300a49c6831ffbdffa
Peter Palaga <ppalaga@redhat.com>
2015-07-16 18:03:55 +0200

    Upgrade to hawkular-parent 18

--- hawkular-alerts-api/src/main/java/org/hawkular/alerts/api/log/MsgLogger.java
+++ hawkular-alerts-api/src/main/java/org/hawkular/alerts/api/log/MsgLogger.java
@@ -16,9 +16,10 @@
  */
 package org.hawkular.alerts.api.log;
 
+import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
-import org.jboss.logging.Message;
 import org.jboss.logging.annotations.LogMessage;
+import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
 import org.jboss.logging.annotations.ValidIdRange;
 
@@ -29,7 +30,7 @@ import org.jboss.logging.annotations.ValidIdRange;
  */
 @MessageLogger(projectCode = "HAWKALERT")
 @ValidIdRange(min = 200000, max = 209999)
-public interface MsgLogger {
+public interface MsgLogger extends BasicLogger {
     MsgLogger LOGGER = Logger.getMessageLogger(MsgLogger.class, MsgLogger.class.getPackage().getName());
 
     @LogMessage(level = Logger.Level.WARN)
