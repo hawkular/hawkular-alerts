56228d0267cce17dfd3ff97e8d6073c501d5b587
Lucas Ponce <ponce.ballesteros@gmail.com>
2016-02-09 11:25:34 +0100

    Fix data not deleted in itest

--- hawkular-alerts-rest-tests/src/test/groovy/org/hawkular/alerts/rest/DampeningITest.groovy
+++ hawkular-alerts-rest-tests/src/test/groovy/org/hawkular/alerts/rest/DampeningITest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -69,6 +69,9 @@ class DampeningITest extends AbstractITestBase {
         assertEquals("test-trigger-6", resp.data[0].triggerId)
 
         resp = client.delete(path: "triggers/test-trigger-6/dampenings/" + d.getDampeningId())
+        assertEquals(200, resp.status)
+
+        client.delete(path: "triggers/test-trigger-6")
         assertEquals(200, resp.status)
     }
 
