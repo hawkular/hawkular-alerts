f6d6478412dd9c5c04cee8d34952e16e9c288731
Lucas Ponce <ponce.ballesteros@gmail.com>
2017-07-14 16:10:17 +0200

    Add default values for match/mode when Trigger is loaded from Ispn (#390)

--- api/src/main/java/org/hawkular/alerts/api/model/trigger/Trigger.java
+++ api/src/main/java/org/hawkular/alerts/api/model/trigger/Trigger.java
@@ -283,8 +283,8 @@ public class Trigger implements Serializable {
         this.source = trigger.getSource();
         this.severity = trigger.getSeverity();
 
-        this.match = trigger.getMatch();
-        this.mode = trigger.getMode();
+        this.mode = trigger.getMode() != null ? trigger.getMode() : Mode.FIRING;
+        this.match = trigger.getMode() == Mode.FIRING ?  trigger.getFiringMatch() : trigger.getAutoResolveMatch();
     }
 
     public Trigger(String tenantId, String id, String name, Map<String, String> context, Map<String, String> tags) {
