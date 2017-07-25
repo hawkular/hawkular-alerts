7d728437814297fb7c66a8baf66301181d2c9080
John Mazzitelli <mazz@redhat.com>
2017-07-25 15:50:12 -0400

    load alerts into scope for display in alerts list (#408)

--- ui/src/main/ui/src/alerts/alerts-controller.js
+++ ui/src/main/ui/src/alerts/alerts-controller.js
@@ -1,8 +1,34 @@
-angular.module('hwk.alertsModule').controller( 'hwk.alertsController', ['$scope', '$rootScope', '$resource', '$window', '$interval', '$q', 'hwk.dashboardService',
-  function ($scope, $rootScope, $resource, $window, $interval, $q, dashboardService) {
+angular.module('hwk.alertsModule').controller( 'hwk.alertsController', ['$scope', '$rootScope', '$resource', '$window', '$interval', '$q', 'hwk.alertsService',
+  function ($scope, $rootScope, $resource, $window, $interval, $q, alertsService) {
     'use strict';
 
-    console.log("[Alerts] $rootScope.selectedTenant " + $rootScope.selectedTenant);
+    var selectedTenant = $rootScope.selectedTenant;
+
+    console.log("[Alerts] $rootScope.selectedTenant " + selectedTenant);
+
+    var watchRef = $rootScope.$watch('selectedTenant', function (newTenant, oldTenant) {
+      selectedTenant = newTenant;
+      console.log('[Alerts] New Tenant: ' + selectedTenant);
+      if (selectedTenant && selectedTenant.length > 0) {
+        updateAlerts();
+      }
+    });
+
+    $scope.$on('$destroy', function() {
+      watchRef();
+    });
+
+    var updateAlerts = function () {
+      if (selectedTenant && selectedTenant.length > 0) {
+        var alertsPromise = alertsService.Alert($rootScope.selectedTenant).query();
+        $q.all([alertsPromise.$promise]).then(function(results) {
+          $scope.alertsList = results[0];
+          console.log("[Alerts] Alerts query returned [" + $scope.alertsList.length + "] alerts");
+        }, function(err) {
+          console.log("[Alerts] Alerts query failed: " + err);
+        });
+      }
+    };
 
   }
 ]);
