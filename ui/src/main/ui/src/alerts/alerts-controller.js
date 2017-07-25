ed8e3ac59650f3f0b8ff1e96e496a1bf54f922a7
John Mazzitelli <mazz@redhat.com>
2017-07-25 17:28:48 -0400

    get the collapsible rows to work (#409)

--- ui/src/main/ui/src/alerts/alerts-controller.js
+++ ui/src/main/ui/src/alerts/alerts-controller.js
@@ -14,10 +14,6 @@ angular.module('hwk.alertsModule').controller( 'hwk.alertsController', ['$scope'
       }
     });
 
-    $scope.$on('$destroy', function() {
-      watchRef();
-    });
-
     var updateAlerts = function () {
       if (selectedTenant && selectedTenant.length > 0) {
         var alertsPromise = alertsService.Alert($rootScope.selectedTenant).query();
@@ -30,5 +26,62 @@ angular.module('hwk.alertsModule').controller( 'hwk.alertsController', ['$scope'
       }
     };
 
+    $scope.$on('ngRepeatDone', function(ngRepeatDoneEvent) {
+      // row checkbox selection
+      $("input[type='checkbox']").change(function (e) {
+        if ($(this).is(":checked")) {
+          $(this).closest('.list-group-item').addClass("active");
+        } else {
+          $(this).closest('.list-group-item').removeClass("active");
+        }
+      });
+
+      // toggle dropdown menu
+      $(".list-view-pf-actions").on("show.bs.dropdown", function () {
+        var $this = $(this);
+        var $dropdown = $this.find(".dropdown");
+        var space = $(window).height() - $dropdown[0].getBoundingClientRect().top - $this.find(".dropdown-menu").outerHeight(true);
+        $dropdown.toggleClass("dropup", space < 10);
+      });
+
+      // compound expansion
+      $(".list-view-pf-expand").on("click", function () {
+        var $this = $(this);
+        var $heading = $(this).parents(".list-group-item");
+        var $subPanels = $heading.find(".list-group-item-container");
+        var index = $heading.find(".list-view-pf-expand").index(this);
+
+        // remove all active status
+        $heading.find(".list-view-pf-expand.active").find(".fa-angle-right").removeClass("fa-angle-down")
+          .end().removeClass("active")
+            .end().removeClass("list-view-pf-expand-active");
+        // add active to the clicked item
+        $(this).addClass("active")
+          .parents(".list-group-item").addClass("list-view-pf-expand-active")
+            .end().find(".fa-angle-right").addClass("fa-angle-down");
+        // check if it needs to hide
+        if ($subPanels.eq(index).hasClass("hidden")) {
+          $heading.find(".list-group-item-container:visible").addClass("hidden");
+          $subPanels.eq(index).removeClass("hidden");
+        } else {
+          $subPanels.eq(index).addClass("hidden");
+          $heading.find(".list-view-pf-expand.active").find(".fa-angle-right").removeClass("fa-angle-down")
+           .end().removeClass("active")
+            .end().removeClass("list-view-pf-expand-active");
+        }
+      });
+
+      // click close button to close the panel
+      $(".list-group-item-container .close").on("click", function () {
+        var $this = $(this);
+        var $panel = $this.parent();
+
+        // close the container and remove the active status
+        $panel.addClass("hidden")
+          .parent().removeClass("list-view-pf-expand-active")
+            .find(".list-view-pf-expand.active").removeClass("active")
+              .find(".fa-angle-right").removeClass("fa-angle-down");
+      });
+    });
   }
 ]);
--- ui/src/main/ui/src/alerts/alerts-module.js
+++ ui/src/main/ui/src/alerts/alerts-module.js
@@ -1,2 +1,17 @@
 
-angular.module( 'hwk.alertsModule', []);
+var alertsModule = angular.module( 'hwk.alertsModule', []);
+
+alertsModule.directive('repeatDone', function($timeout) {
+  'use strict';
+
+  return {
+    restrict: 'A',
+    link: function (scope, element, attr) {
+      if (scope.$last === true) {
+        $timeout(function () {
+          scope.$emit('ngRepeatDone');
+        }, 0);
+      }
+    }
+  };
+});
